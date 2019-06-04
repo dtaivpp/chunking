a2e655d372bf3a310b9ef1e2b6476d95b5f3e682
David Tippett <dtippett@Davids-MacBook-Pro-5.local>
2019-06-04 11:48:53 -0400

    Comments on methodology.

--- chunking_tests.js
+++ chunking_tests.js
@@ -3,6 +3,11 @@ var max = 100;
 var timevalues = {"chunkFor":0,"chunkForChunks":0,"chunkSlice":0,"chunkRecurse":0};
 
 function chunkingTests(randArr){
+  /**
+   * Run each method with the array recording the time the solution
+   * took to run. 
+   */
+
   var timestamp = Date.now();
   chunking.chunkFor(randArr, 20);
   timevalues.chunkFor += Date.now() - timestamp;
@@ -21,14 +26,20 @@ function chunkingTests(randArr){
 }
 
 function randomArr(){
+  /**
+   * Creates a random array with between 1 and 70,000 random numbers.
+   */
+
   let randArr = [...Array(~~(Math.random()*70000))].map(e=>~~(Math.random()*100));
   return randArr
 }
 
+// Runs each method with a new random array
 for (var i = 0; i < max; i++){
   chunkingTests(randomArr());
 }
 
+// Log Results
 console.log("chunkFor: ",timevalues.chunkFor / max, " ms");
 console.log("chunkForChunks: ",timevalues.chunkForChunks / max, " ms");
 console.log("chunkSlice: ",timevalues.chunkSlice / max, " ms");
